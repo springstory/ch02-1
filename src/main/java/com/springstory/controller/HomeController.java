f717a0dd73ee71b7a70624b011176b9fa0c9198a
hamzang <itaeho@me.com>
2014-07-12 10:32:36 +0900

    last

--- src/main/java/com/springstory/controller/HomeController.java
+++ src/main/java/com/springstory/controller/HomeController.java
@@ -1,13 +1,10 @@
 package com.springstory.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,18 +21,15 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
-	@Qualifier("userServiceImpl2")
 	private UserService userService;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/hello2.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/hello.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		
 		String message = userService.getHelloMessage();
 		
-		logger.info("Welcome home! The client locale is {}.", locale);
-	
 		model.addAttribute("message", message );
 		
 		return "home";
--- src/main/java/com/springstory/service/UserServiceImpl.java
+++ src/main/java/com/springstory/service/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.springstory.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+import com.springstory.dao.UserDao;
+
+@Service("userService")
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private UserDao userDao;
+	
 	public String getHelloMessage() {
-		return "¾È³çÇÏ¼¼¿ä";
+		return "ì•ˆë…•í•˜ì„¸ìš” " + userDao.getUser("hamzang").getName();
 	}
 }
